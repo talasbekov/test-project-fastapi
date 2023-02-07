dca28277db489da087273d1be7ab46529d54c103
talasbekov <talasbekoverdaulet@gmail.com>
2023-02-07 21:05:12 +0600

    fix: added user router

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -5,6 +5,7 @@ from .hr_document import router as hr_document_router
 from .user_stat import router as user_stat_router
 from .position import router as position_router
 from .permission import router as permission_router
+from .user import router as user_router
 
 router = APIRouter(prefix="/v1")
 
@@ -12,3 +13,4 @@ router.include_router(hr_document_router)
 router.include_router(user_stat_router)
 router.include_router(position_router)
 router.include_router(permission_router)
+router.include_router(user_router)
