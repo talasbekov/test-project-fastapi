1c7a0531fbb52bbfe68c195bda3f97335a2ec243
Serikzhan Kuanyshev <70684876+SekoJuku@users.noreply.github.com>
2023-02-13 13:32:14 +0600

    fix: schemas for None

--- schemas/hr_document_template.py
+++ schemas/hr_document_template.py
@@ -1,7 +1,7 @@
 import uuid
 
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Union
 
 from models import SubjectType
 
@@ -10,8 +10,8 @@ class HrDocumentTemplateBase(BaseModel):
     name: str
     path: str
     subject_type: SubjectType
-    properties: dict
-    details: dict
+    properties: Union[dict, None]
+    details: Union[dict, None]
 
 
 class HrDocumentTemplateCreate(HrDocumentTemplateBase):
