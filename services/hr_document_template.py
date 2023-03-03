854ec13ba38fbb2989db0826a61ce82df447473c
Serikzhan Kuanyshev <kuanyshev@mail.ru>
2023-03-03 19:44:36 +0600

    feat (services): add get steps with document template

--- services/hr_document_template.py
+++ services/hr_document_template.py
@@ -5,12 +5,17 @@ from fastapi.logger import logger as log
 from sqlalchemy.orm import Session
 
 from exceptions import NotFoundException
-from models import HrDocumentTemplate
+from models import HrDocumentTemplate, HrDocumentStep, DocumentStaffFunction, User
 from schemas import HrDocumentTemplateCreate, HrDocumentTemplateUpdate
 
 from .base import ServiceBase
 
-
+"""
+{
+  "1": "uuid",
+  "2": "uuid"
+}
+"""
 class HrDocumentTemplateService(ServiceBase[HrDocumentTemplate, HrDocumentTemplateCreate, HrDocumentTemplateUpdate]):
 
     def get_by_id(self, db: Session, id: str) -> HrDocumentTemplate:
@@ -18,6 +23,24 @@ class HrDocumentTemplateService(ServiceBase[HrDocumentTemplate, HrDocumentTempla
         if hr_document_template is None:
             raise NotFoundException(detail=f'HrDocumentTemplate with id: {id} is not found!')
         return hr_document_template
+    def get_steps_by_document_template_id(self, db: Session, document_template_id: str) -> HrDocumentTemplate:
+        
+        all_steps = db.query(DocumentStaffFunction).filter(
+            HrDocumentStep.hr_document_template_id == document_template_id,
+            DocumentStaffFunction.priority != 1
+        ).join(HrDocumentStep.staff_function).order_by(DocumentStaffFunction.priority.asc()).all()
+         
+        steps = {}
+        for function in all_steps:
+            function: DocumentStaffFunction
+            staff_units_ids = [unit.id for unit in function.staff_units]
 
+            user = db.query(User).filter(
+                User.staff_unit_id.in_(staff_units_ids)
+            ).first()
+            steps[str(function.priority)] = str(user.id)
+        print(steps)
+        return steps
+ 
 
 hr_document_template_service = HrDocumentTemplateService(HrDocumentTemplate)
