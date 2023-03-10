5261c72465674aaf04fca2cf9cbecc00fe66acb1
Serikzhan Kuanyshev <kuanyshev@mail.ru>
2023-03-10 20:56:11 +0600

    feat (schemas): set list of user_vehicles

--- schemas/additional/__init__.py
+++ schemas/additional/__init__.py
@@ -14,7 +14,7 @@ from .service_housing import (ServiceHousingUpdate, ServiceHousingCreate,
                               ServiceHousingRead)
 from .properties import (PropertiesBase, PropertiesCreate,
                          PropertiesRead, PropertiesUpdate)
-from .additional_profile import (AdditionalProfileCreate, AdditionalProfileUpdate,
-                                 AdditionalProfileRead)
 from .vehicle import (VehicleBase, VehicleCreate, VehicleRead, 
                       VehicleUpdate)
+from .additional_profile import (AdditionalProfileCreate, AdditionalProfileUpdate,
+                                 AdditionalProfileRead)
