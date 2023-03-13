9f4df86239d7cfc6ee7c8a39f269df9a45b95dfc
Yernar Ayanuly <erjan.ernar2002@gmail.com>
2023-03-13 11:56:54 +0600

    delete: method get by user id

--- services/profile/personal/personal_profile.py
+++ services/profile/personal/personal_profile.py
@@ -14,12 +14,6 @@ class PersonalProfileService(ServiceBase[PersonalProfile, PersonalProfileCreate,
         if personal_profile is None:
             raise NotFoundException(detail=f"PersonalProfile with id: {id} is not found!")
         return personal_profile
-    
-    def get_by_user_id(self, db: Session, user_id: str) -> PersonalProfile:
-        res = db.query(self.model).join(Profile).filter(Profile.user_id == user_id).first()
-        if res is None:
-            raise NotFoundException(detail=f'PersonalProfile for user_id: {user_id} is not found!')
-        return res
 
 
 personal_profile_service = PersonalProfileService(PersonalProfile)
