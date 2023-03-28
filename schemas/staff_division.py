e562fc10de98ecbdfe701c4608d15ffd11a2c1f0
Serikzhan Kuanyshev <70684876+SekoJuku@users.noreply.github.com>
2023-03-28 12:18:37 +0600

    fix: schemas for staff_division

--- schemas/staff_division.py
+++ schemas/staff_division.py
@@ -37,15 +37,48 @@ class StaffDivisionRead(StaffDivisionBase):
         orm_mode = True
 
 
+class UserRead(BaseModel):
+    id: Optional[uuid.UUID]
+    rank: Optional[RankRead]
+    email: Optional[EmailStr]
+    first_name: Optional[str]
+    last_name: Optional[str]
+    staff_unit_id: Optional[uuid.UUID]
+    call_sign: Optional[str]
+    id_number: Optional[str]
+    status: Optional[str]
+    status_till: Optional[datetime.datetime]
+
+    class Config:
+        orm_mode = True
+
+
+class StaffUnitRead(BaseModel):
+    
+    id: Optional[uuid.UUID]
+    staff_division_id: Optional[uuid.UUID]
+    position_id: Optional[uuid.UUID]
+    position: Optional[PositionRead]
+    users: Optional[List[Optional[UserRead]]]
+    actual_users: Optional[List[Optional[UserRead]]]
+
+    class Config:
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+
 class StaffDivisionOptionRead(StaffDivisionBase):
 
     id: Optional[uuid.UUID]
     name: Optional[str]
-    staff_units: Optional[List]
+    is_combat_unit: Optional[bool]
+    staff_units: Optional[List[StaffUnitRead]]
     children: Optional[List['StaffDivisionOptionRead']]
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
+
 
 class StaffUnitDivisionRead(StaffDivisionBase):
     id: Optional[uuid.UUID]
