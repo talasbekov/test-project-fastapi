a4cd4368757ebaebb5a250f468e89833abca9753
Ayan Sultan <oiansheks@gmail.com>
2023-04-07 14:48:49 +0600

    edit get_profile in profile service

--- services/profile/profile.py
+++ services/profile/profile.py
@@ -11,14 +11,25 @@ class ProfileService(ServiceBase[Profile, ProfileCreate, ProfileUpdate]):
     def get_by_id(self, db: Session, id: str):
         profile = super().get(db, id)
         if profile is None:
-            raise NotFoundException(detail=f"Profile with id: {id} is not found!")
+            raise NotFoundException(
+                detail=f"Profile with id: {id} is not found!")
         return profile
-    
+
     def get_by_user_id(self, db: Session, id: str) -> Profile:
-        profile = db.query(self.model).filter(self.model.user_id == id).first() 
+        profile = db.query(self.model).filter(self.model.user_id == id).first()
         if profile is None:
-            raise NotFoundException(detail=f"Profile with user_id: {id} is not found!")
+            raise NotFoundException(
+                detail=f"Profile with user_id: {id} is not found!")
         return profile
+
+    def generate_profile_doc(self, db: Session, user_id: str):
+        profile = self.get_by_user_id(db, user_id)
+
+        data = {
+
+        }
+
+        return
 
 
 profile_service = ProfileService(Profile)
