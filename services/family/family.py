f53aaae89901a39885df66cc04eeba7f6ea80d1c
Ernar2002 <erjan.ernar2002@gmail.com>
2023-04-14 13:35:22 +0300

    fix/render service

--- services/family/family.py
+++ services/family/family.py
@@ -13,6 +13,12 @@ class FamilyService(ServiceBase[Family, FamilyCreate, FamilyUpdate]):
         if not family:
             raise NotFoundException("Family with id: {id} not found!")
         return family
+    
+    def get_by_relation_id(self, db: Session, relation_id: str):
+        family = db.query(Family).filter(Family.relation_id == relation_id).first()
+        if not family:
+            raise NotFoundException("Family with relation_id: {relation_id} not found!")
+        return family
 
 
 family_service = FamilyService(Family)
--- services/render.py
+++ services/render.py
@@ -1,12 +1,16 @@
 import tempfile
 import urllib.parse
 import urllib.request
+import datetime
 
 from docxtpl import DocxTemplate
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
-from services import (hr_document_template_service, candidate_service, staff_unit_service)
+from services import (hr_document_template_service, candidate_service, staff_unit_service,
+                      profile_service, family_profile_service, family_relation_service,
+                      family_service)
+from models import User, Profile
 
 
 class RenderService:
@@ -16,11 +20,21 @@ class RenderService:
 
         candidate_staff_unit = staff_unit_service.get_by_id(db, candidate['staff_unit_id'])
 
-        candidate_user = candidate_staff_unit.users[0]
+        candidate_user: User = candidate_staff_unit.users[0]
 
         curator_staff_unit = staff_unit_service.get_by_id(db, candidate['staff_unit_curator_id'])
 
-        curator_user = curator_staff_unit.users[0]
+        curator_user: User = curator_staff_unit.users[0]
+
+        profile: Profile = profile_service.get_by_user_id(db, candidate_user.id)
+
+        family_profile = family_profile_service.get_by_profile_id(db, profile.id)
+
+        father_relation = family_relation_service.get_by_name(db, "Отец")
+        mother_relation = family_relation_service.get_by_name(db, "Мать")
+
+        father = family_service.get_by_relation_id(db, father_relation.id)
+        mother = family_service.get_by_relation_id(db, mother_relation.id)
 
         document_template = hr_document_template_service.get_by_id(
             db, template_id
@@ -35,12 +49,24 @@ class RenderService:
 
         template = DocxTemplate(temp_file_path)
 
+        now = datetime.datetime.now()
+
+        month_num = now.month
+
+        month_names = [
+            'Қаңтар', 'Ақпан', 'Наурыз', 'Сәуір', 'Мамыр', 'Маусым', 'Шілде', 'Тамыз', 'Қыркүйек', 'Қазан', 'Қараша', 'Желтоқсан'
+        ]
+
+        month_name = month_names[month_num - 1]
+
         context = {
             'candidate': candidate_user,
             'curator': curator_user,
-            'year': 2023,
-            'month': 'December',
-            'day': 1
+            'year': now.year,
+            'month': month_name,
+            'day': now.day,
+            'father': father,
+            'mother': mother
         }
 
         template.render(context)
