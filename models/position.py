4efc8a83d7af52db47d7ef6e773312700e246aec
Krulwaifu <super.raha2002@gmail.com>
2023-05-25 21:31:58 +0600

    fix: position rank

--- models/position.py
+++ models/position.py
@@ -34,5 +34,8 @@ class Position(NamedModel):
 
     __tablename__ = "positions"
 
+    max_rank_id = Column(UUID(as_uuid=True), ForeignKey("ranks.id"),
+                         nullable=True)
+
     rank = relationship("Rank", cascade="all,delete")
     category_code = Column(String, nullable=False)
