0d8eb2309457ee0a786e0d139e550f8dd805970c
rruss <rrussarybayev@gmail.com>
2023-05-25 19:34:29 +0600

    feat: max rank

--- schemas/position.py
+++ schemas/position.py
@@ -20,7 +20,7 @@ class PositionUpdate(PositionBase):
 
 class PositionRead(PositionBase, ReadNamedModel):
     category_code: Optional[str]
-    max_rank_id: Optional[uuid.UUID]
+    max_rank: Optional[RankRead]
     rank: Optional[RankRead]
 
     class Config:
