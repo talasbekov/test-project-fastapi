fea99fa45fe9d595dfa3f8d79661122b07e5edfb
Ernar2002 <erjan.ernar2002@gmail.com>
2023-06-13 10:17:39 +0300

    fix/profile/family-status

--- services/profile/personal/family_status.py
+++ services/profile/personal/family_status.py
@@ -1,8 +1,10 @@
+from ast import Pass
+from math import e
 from sqlalchemy.orm import Session
 
 from exceptions.client import NotFoundException
 from models import FamilyStatus
-from schemas import FamilyStatusCreate, FamilyStatusUpdate
+from schemas import FamilyStatusCreate, FamilyStatusUpdate, FamilyStatusRead
 from services.base import ServiceBase
 from services.profile import profile_service
 
@@ -15,7 +17,21 @@ class FamilyStatusService(ServiceBase[FamilyStatus, FamilyStatusCreate, FamilySt
         return family_status
     
     def get_by_user_id(self, db: Session, id: str):
-        return profile_service.get_by_user_id(db, id).personal_profile
+        profile = profile_service.get_by_user_id(db, id)
+        
+        try:        
+            res = FamilyStatusRead.from_orm(profile.personal_profile.biographic_info.family_status).dict()
+        except:
+            return None
+        
+        if profile.personal_profile.biographic_info.gender == 1:
+            res['name'] = res['name'].split(' / ')[0]
+            res['nameKZ'] = res['nameKZ'].split(' / ')[0]
+        else:
+            res['name'] = res['name'].split(' / ')[1]
+            res['nameKZ'] = res['nameKZ'].split(' / ')[1]
+        
+        return res
 
 
 family_status_service = FamilyStatusService(FamilyStatus)
