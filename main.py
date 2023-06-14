69d3d9ad0d9bdd8def1e3494c6b974c433956d75
“Kamipedia” <camilazhumakhanova@gmail.com>
2023-06-15 02:13:50 +0600

    refactor: delete debug middleware

--- main.py
+++ main.py
@@ -2,25 +2,21 @@ import time
 import socket
 
 import sentry_sdk
-from fastapi import Depends, FastAPI, Request, WebSocket, WebSocketDisconnect, Query, Header
+from fastapi import Depends, FastAPI, Request, WebSocket, WebSocketDisconnect, Query
 from fastapi.logger import logger as log
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException, JWTDecodeError
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
-from debug_toolbar.middleware import DebugToolbarMiddleware
 
 
 from api import router
-from core import configs, get_db, download_file_to_tempfile
+from core import configs
 from ws import notification_manager
 
 
-socket.setdefaulttimeout(15)
+socket.setdefaulttimeout(15) # TODO: change to configs.SOCKET_TIMEOUT
 
 app = FastAPI(
     title=configs.PROJECT_NAME,
@@ -32,22 +28,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # TODO: change to configs.ALLOWED_HOSTS
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
-# sqlalchemy
-app.add_middleware(
-    DebugToolbarMiddleware,
-    panels=[
-        "debug_toolbar.panels.versions.VersionsPanel",
-        "debug_toolbar.panels.timer.TimerPanel",
-        "debug_toolbar.panels.settings.SettingsPanel",
-        "debug_toolbar.panels.headers.HeadersPanel",
-        "debug_toolbar.panels.request.RequestPanel",
-        "debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel",
-    ],
 )
 
 app.include_router(router)
@@ -59,11 +43,6 @@ if configs.SENTRY_ENABLED:
 @AuthJWT.load_config
 def get_config():
     return configs
-
-
-@app.get("/test")
-async def test(db: Session = Depends(get_db)):
-    return []
 
 
 @app.middleware("http")
@@ -95,11 +74,6 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 @app.get("/", include_in_schema=False)
 async def docs_redirect():
     return RedirectResponse(url="/docs")
-
-
-@app.get("/sentry-debug")
-async def trigger_error():
-    division_by_zero = 1 / 0
 
 
 @app.websocket("/ws")
