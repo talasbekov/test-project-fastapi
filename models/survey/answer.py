b39f117311b5818f64df754112201f23fd5cc83a
Ernar2002 <erjan.ernar2002@gmail.com>
2023-06-18 14:23:08 +0600

    fix survey: models

--- models/survey/answer.py
+++ models/survey/answer.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, ForeignKey, String, TEXT, Integer, JSON
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -11,6 +11,59 @@ class Answer(Model):
     __tablename__ = "answers"
 
     question_id = Column(UUID(as_uuid=True), ForeignKey("survey_types.id"))
+    discriminator = Column(String(255))
 
     question = relationship("Question", foreign_keys=[question_id], back_populates="answers")
+    
+    __mapper_args__ = {
+        "polymorphic_on": discriminator,
+        "polymorphic_identity": "answers"
+    }
+    
+
+class AnswerText(Answer):
+    text = Column(TEXT, nullable = True)
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "answer_text"
+    }
+
+
+class AnswerSingleChoice(Answer):
+    option_id = Column(UUID(as_uuid=True), ForeignKey("options.id"), nullable = True)
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "answer_single_choice"
+    }
+
+
+class AnswerMultipleChoice(Answer):
     options = relationship("Option", secondary=answers_options, back_populates="answers")
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "answer_multiple_choice"
+    }
+    
+
+class AnswerScale(Answer):
+    scale_value = Column(Integer, nullable=True)
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "answer_scale"
+    }
+    
+
+class AnswerGrid(Answer):
+    grid_values = Column(JSON, nullable=True)
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "answer_grid"
+    }
+    
+
+class AnswerCheckboxGrid(Answer):
+    checkbox_grid_values = Column(JSON, nullable=True)
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'answer_checkbox_grid',
+    }
--- models/survey/option.py
+++ models/survey/option.py
@@ -11,22 +11,22 @@ class Option(Model):
     __tablename__ = "options"
 
     question_id = Column(UUID(as_uuid=True), ForeignKey("questions.id"))
+    text = Column(TEXT, nullable = True)
     discriminator = Column(String(255))
 
     question = relationship("Question", foreign_keys=[question_id], back_populates="options")
-    answers = relationship("Answer", secondary=answers_options, back_populates="answers")
 
     __mapper_args__ = {
         "polymorphic_on": discriminator,
         "polymorphic_identity": "options"
     }
+    
 
-
-class OptionText(Option):
-    text = Column(TEXT, nullable = True)
+class OptionMultipleChoice(Option):
+    answers = relationship("Answer", secondary=answers_options, back_populates="answers")
 
     __mapper_args__ = {
-        "polymorphic_identity": "option_text"
+        "polymorphic_identity": "option_multiple_choice"
     }
 
 
