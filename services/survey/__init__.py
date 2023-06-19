025b14c38bc76b12a5058da19072b381d8526406
Ernar2002 <erjan.ernar2002@gmail.com>
2023-06-19 17:30:51 +0600

    feature survey: base services

---
+++ services/survey/__init__.py
@@ -0,0 +1,3 @@
+from .survey import survey_service
+from .option import option_service
+from .question import question_service
---
+++ services/survey/question.py
@@ -0,0 +1,15 @@
+from models import Question
+from schemas import (QuestionCreate, QuestionUpdate,
+                     QuestionRead)
+from services.base import ServiceBase
+
+
+class QuestionService(ServiceBase[Question, QuestionCreate, QuestionUpdate]):
+    
+    def get_by_survey(self, db, survey_id):
+        return db.query(self.model).filter(
+            self.model.survey_id == survey_id
+        ).all()
+
+
+question_service = QuestionService(Question)
