f165e79d0cda93e5c8f11760399a780e6b07bdd8
Ernar2002 <erjan.ernar2002@gmail.com>
2023-07-13 16:10:54 +0600

    fix(candidate): action of finish candidate

--- services/constructor/position_change.py
+++ services/constructor/position_change.py
@@ -3,10 +3,13 @@ import logging
 from sqlalchemy.orm import Session
 
 from core import configs
-from models import User, HrDocument, EmergencyServiceHistory
+from models import (User, HrDocument, EmergencyServiceHistory,
+                    StaffDivisionEnum, CandidateStatusEnum)
 from schemas import StaffUnitRead
 from .base import BaseHandler
-from services import staff_unit_service, history_service
+from services import (staff_unit_service, history_service,
+                      staff_division_service)
+from services.candidates import candidate_service
 from exceptions import BadRequestException
 
 
@@ -24,7 +27,26 @@ class PositionChangeHandler(BaseHandler):
     ):
         self.handle_validation(
             db, user, action, template_props, props, document)
-        position_id, percent, reason = self.get_args(action, props)
+        
+        candidate_staff_division = (
+            staff_division_service.get_by_name(
+                db, StaffDivisionEnum.CANDIDATES.value)
+        )
+        
+        if user.staff_unit.staff_division_id == candidate_staff_division.id:
+            candidate = candidate_service.get_by_staff_unit_id(db, user.staff_unit_id)
+            
+            position_id = self.get_args(action, props)[0]
+            
+            percent = None
+            reason = None
+            
+            candidate.status = CandidateStatusEnum.COMPLETED.value
+            
+            db.add(candidate)
+        else:
+            position_id, percent, reason = self.get_args(action, props)
+        
         old_history = staff_unit_service.get_last_history(db, user.id)
 
         if old_history is None:
@@ -59,18 +81,27 @@ class PositionChangeHandler(BaseHandler):
         props: dict,
         document: HrDocument,
     ):
-        position_id, percent, reason = self.get_args(action, props)
+        candidate_staff_division = (
+            staff_division_service.get_by_name(
+                db, StaffDivisionEnum.CANDIDATES.value)
+        )
+        
+        position_id = self.get_args(action, props)[0]
+        
+        if user.staff_unit.staff_division_id != candidate_staff_division.id:
+            percent = self.get_args(action, props)[1]
 
-        staff_unit_service.get_by_id(db, position_id)
-
+            staff_unit_service.get_by_id(db, position_id)
+            
+            if percent < 0 or percent > 100:
+                raise BadRequestException(
+                    f"Percentage must be between 0 and 100: {percent}")
+        
         if staff_unit_service.exists_relation(db, user.id, position_id):
             raise BadRequestException(
                 ("This position is already assigned to this user:"
-                 f" {user.first_name}, {user.last_name}")
+                f" {user.first_name}, {user.last_name}")
             )
-        if percent < 0 or percent > 100:
-            raise BadRequestException(
-                f"Percentage must be between 0 and 100: {percent}")
 
     def get_args(self, action, properties):
         try:
