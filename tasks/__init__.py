d46a88904e00774b7e43978bb191a54c5af05ee0
Ernar2002 <erjan.ernar2002@gmail.com>
2023-07-20 21:18:59 +0600

    feature(survey): repeat celery task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,1 +1,6 @@
-from .celery_app import task_create_draft, task_apply_staff_list
+from .celery_app import (
+    task_create_draft,
+    task_apply_staff_list,
+    task_clear_user_logging_activities_table_every_3_days,
+    task_repeat_surveys
+)
--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -8,13 +8,14 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
 from core import configs
-from services.staff_list import staff_list_service
+from services import staff_list_service, survey_service
 from schemas.staff_list import StaffListUserCreate, StaffListRead
-from models import UserLoggingActivity
+from models import UserLoggingActivity, SurveyRepeatTypeEnum
 
 
 app = Celery('celery_app', backend='redis://redis:6379/',
-             broker='amqp://rabbitmq:5672/', timezone='Asia/Almaty')
+             broker='amqp://rabbitmq:5672/', timezone='Asia/Almaty',
+             include=['tasks.celery_app'])
 
 
 app.conf.beat_schedule = {
@@ -22,6 +23,21 @@ app.conf.beat_schedule = {
         'task': 'tasks.celery_app.task_clear_user_'+
                     'logging_activities_table_every_3_days',
         'schedule': crontab(minute=0, hour=0, day_of_month='*/3')
+    },
+    'repeat_surveys_with_every_week_type': {
+        'task': 'tasks.celery_app.task_repeat_surveys',
+        'schedule': crontab(minute=10, hour=21, day_of_week='thu'),
+        'args': (SurveyRepeatTypeEnum.EVERY_WEEK.value,)
+    },
+    'repeat_surveys_with_every_month_type': {
+        'task': 'tasks.celery_app.task_repeat_surveys',
+        'schedule': crontab(minute=0, hour=0, day_of_month='1'),
+        'args': (SurveyRepeatTypeEnum.EVERY_MONTH.value,)
+    },
+    'repeat_surveys_with_every_year_type': {
+        'task': 'tasks.celery_app.task_repeat_surveys',
+        'schedule': crontab(minute=0, hour=0, day_of_month='1', month_of_year='1'),
+        'args': (SurveyRepeatTypeEnum.EVERY_YEAR.value,)
     }
 }
 
@@ -107,6 +123,28 @@ def task_clear_user_logging_activities_table_every_3_days():
         
         for obj in delete_objects:
             db.delete(obj)
+        
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        if db:
+            db.close()
+    
+    return True
+
+@app.task
+def task_repeat_surveys(status: str):
+    SessionLocal = sessionmaker(bind=engine)
+    db = SessionLocal()
+    try:
+        expired_surveys = (
+            survey_service.get_expired_by_repeat_type(db, status)
+        )
+        
+        for survey in expired_surveys:
+            survey_service.repeat(db, survey.id)
         
         db.commit()
     except Exception as e:
