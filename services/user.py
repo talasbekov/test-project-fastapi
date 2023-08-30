4962ecd30368618d500a365da0af37dc11177780
Ernar2002 <erjan.ernar2002@gmail.com>
2023-08-30 13:57:13 +0600

    fix(users): get all by candidate subject type

--- services/user.py
+++ services/user.py
@@ -20,7 +20,7 @@ from models import (
     HrDocumentInfo,
     HrDocumentTemplate,
     ScheduleYear,
-    Rank
+    SubjectType
 )
 from schemas import (
     UserCreate,
@@ -66,6 +66,10 @@ class UserService(ServiceBase[User, UserCreate, UserUpdate]):
                 filter: str,
                 skip: int,
                 limit: int) -> List[User]:
+        template = hr_document_template_service.get_by_id(
+            db, hr_document_template_id
+        )
+        
         users = (
             db.query(self.model)
         )
@@ -80,7 +84,19 @@ class UserService(ServiceBase[User, UserCreate, UserUpdate]):
                                                    users,
                                                    hr_document_template_id)
                      .except_(excepted_users)
-                     .filter(self.model.is_active == True))
+                     .filter(self.model.is_active.is_(True)))
+            
+        if template.subject_type == SubjectType.CANDIDATE.value:
+            candidate_staff_division = (
+                staff_division_service.get_by_name(
+                    db, StaffDivisionEnum.CANDIDATES.value
+                )
+            )
+            users = (
+                users
+                .join(StaffUnit, StaffUnit.id == self.model.staff_unit_id)
+                .filter(StaffUnit.staff_division_id == candidate_staff_division.id)
+            )
 
         users = (
             users
@@ -90,6 +106,7 @@ class UserService(ServiceBase[User, UserCreate, UserUpdate]):
             .limit(limit)
             .all()
         )
+
         return users
 
     def is_template_accessible_for_user(self,
