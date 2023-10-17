4ff3d1af02e95f571bbaae5c09757990d63c98e9
usylli <alihangames1@gmail.com>
2023-10-17 14:35:23 +0600

    added all consts

--- services/dictionary.py
+++ services/dictionary.py
@@ -21,15 +21,44 @@ class DictionaryService:
             entity (str): entity which records will be joined
             body (JoinRecordsBody): request body with ids
         """
-        # TODO: write consts for all cases
         TABLES = {
             'institutions': 'hr_erp_educations',
-            'positions': 'hr_erp_positions'
+            'positions': 'hr_erp_staff_units',
+            'ranks': 'hr_erp_users',
+            'badges': 'hr_erp_badges',
+            'penalties': 'hr_erp_penalties',
+            'properties': 'hr_erp_properties',
+            'sports': 'hr_erp_sport_degrees',
+            'statuses': 'hr_erp_statuses',
+            'specialties': 'hr_erp_educations',
+            'academic_degrees': 'hr_erp_academic_degrees',
+            'academic_titles': 'hr_erp_academic_titles',
+            'country': 'hr_erp_abroad_travels',
+            'courses': 'hr_erp_courses',
+            'inst_degrees': 'hr_erp_educations',
+            'institutions': 'hr_erp_educations',
+            'sciences': 'hr_erp_academic_degrees',
+            'languages': 'hr_erp_language_proficiencies'
         }
 
         COLUMNS = {
             'institutions': 'institution_id',
-            'positions': 'hr_erp_positions'
+            'positions': 'position_id',
+            'ranks': 'rank_id',
+            'badges': 'type_id',
+            'penalties': 'type_id',
+            'properties': 'type_id',
+            'sports': 'sport_type_id',
+            'statuses': 'type_id',
+            'specialties': 'specialty_id',
+            'academic_degrees': 'degree_id',
+            'academic_titles': 'degree_id',
+            'country': 'destination_country_id',
+            'courses': 'course_provider_id',
+            'inst_degrees': 'degree_id',
+            'institutions': 'institution_id',
+            'sciences': 'science_id',
+            'languages': 'language_id'
         }
 
         query = text(f"UPDATE {TABLES[entity]} "
@@ -51,14 +80,32 @@ class DictionaryService:
             entity (str): entity which records will be soft updated
             id (str): id to update
         """
-        # TODO: write consts for all cases
         MODULES = {
-            'institution': ('education', 'Institution')
+            'institution': ('education', 'Institution'),
+            'positions': ('Position'),
+            'ranks': ('Rank'),
+            'badges': ('Badge'),
+            'penalties': ('Penalty'),
+            'properties': ('additional', 'Properties'),
+            'sports': ('personal', 'SportType'),
+            'statuses': ('Status'),
+            'specialties': ('education', 'Specialty'),
+            'academic_degrees': ('education', 'AcademicDegreeDegree'),
+            'academic_titles': ('education', 'AcademicTitleDegree'),
+            'country': ('additional', 'Country'),
+            'courses': ('education', 'Course'),
+            'inst_degrees': ('education', 'InstitutionDegreeType'),
+            'sciences': ('education', 'Science'),
+            'languages': ('education', 'Language')
         }
-
-        package, class_name = MODULES[entity]
-        module = __import__('models.' + package,
-                            fromlist=[class_name])
+        if len(MODULES[entity]) == 1:
+            class_name = MODULES[entity]
+            module = __import__('models',
+                                fromlist=[class_name])
+        else:
+            package, class_name = MODULES[entity]
+            module = __import__('models.' + package,
+                                fromlist=[class_name])
         class_obj = getattr(module, class_name)
 
         current_obj = db.query(class_obj).filter(
