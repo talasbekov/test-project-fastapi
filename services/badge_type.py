001d5e53f04a302cbe045965a20a787690be8ad2
krulwaifu <super.raha2002@gmail.com>
2023-11-14 14:57:28 +0600

    feat: badge_type search

--- services/badge_type.py
+++ services/badge_type.py
@@ -1,3 +1,4 @@
+from sqlalchemy import and_, func
 from sqlalchemy.orm import Session
 
 from models import BadgeType
@@ -7,19 +8,38 @@ from .base import ServiceBase
 
 class BadgeTypeService(ServiceBase[BadgeType, BadgeTypeCreate, BadgeTypeUpdate]):
 
-    def get_multi(self, db: Session,
+    def get_all(self, db: Session,
                   skip: int,
-                  limit: int):
+                  limit: int,
+                  filter: str = ''
+                  ):
+        badge_types = db.query(BadgeType)
 
-        badge_types = (db.query(BadgeType)
-                         .order_by(BadgeType.name)
-                         .offset(skip)
-                         .limit(limit)
-                         .all())
+        if filter != '':
+            badge_types = self._add_filter_to_query(badge_types, filter)
+
+        badge_types = (badge_types
+                       .order_by(BadgeType.name)
+                       .offset(skip)
+                       .limit(limit)
+                       .all())
 
         total = (db.query(BadgeType).count())
 
         return {'total': total, 'objects': badge_types}
+
+    def _add_filter_to_query(self, badge_type_query, filter):
+        key_words = filter.lower().split()
+        badge_types = (
+            badge_type_query
+            .filter(
+                and_(func.concat(
+                    func.concat(func.concat(func.lower(BadgeType.name), ' '),
+                                func.concat(func.lower(BadgeType.nameKZ), ' '))
+                    ).contains(name) for name in key_words)
+            )
+        )
+        return badge_types
 
 
 badge_type_service = BadgeTypeService(BadgeType)
