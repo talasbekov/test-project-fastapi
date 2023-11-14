712a09fcb72de9b28897246d96f7879330fdca94
krulwaifu <super.raha2002@gmail.com>
2023-11-14 17:54:34 +0600

    feat: filter CourseProvider

--- api/v1/education/course_provider.py
+++ api/v1/education/course_provider.py
@@ -25,7 +25,8 @@ async def get_all(*,
                   db: Session = Depends(get_db),
                   skip: int = 0,
                   limit: int = 100,
-                  Authorize: AuthJWT = Depends()
+                  Authorize: AuthJWT = Depends(),
+                  filter: str = ''
                   ):
     """
         Get all CourseProviders
@@ -38,7 +39,7 @@ async def get_all(*,
         This parameter is optional and defaults to 100.
     """
     Authorize.jwt_required()
-    return course_provider_service.get_multi(db, skip, limit)
+    return course_provider_service.get_all(db, skip, limit, filter)
 
 
 @router.post("", status_code=status.HTTP_201_CREATED,
