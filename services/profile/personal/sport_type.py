fcee7887ef97a5a4343aed86d6c7c0368c7a8066
krulwaifu <super.raha2002@gmail.com>
2023-11-16 12:26:11 +0600

    feat: sport_degree_type and sport_type added filters

--- services/profile/personal/sport_type.py
+++ services/profile/personal/sport_type.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func
+from sqlalchemy import func, and_
 from sqlalchemy.orm import Session
 
 from exceptions.client import NotFoundException
@@ -16,17 +16,35 @@ class SportTypeService(ServiceBase):
             raise NotFoundException("Sport type not found")
         return sport_type
 
-    def get_multi(
-        self, db: Session, skip: int = 0, limit: int = 100
-    ) -> List[SportType]:
-        sport_types = (db.query(SportType)
-                         .order_by(func.to_char(func.lower(SportType.name)))
-                         .offset(skip)
-                         .limit(limit)
-                         .all())
+    def get_all(
+        self, db: Session, skip: int = 0, limit: int = 100, filter: str = ''
+    ):
+        sport_types = (db.query(SportType))
+
+        if filter != '':
+            sport_types = self._add_filter_to_query(sport_types, filter)
+
+        sport_types = (sport_types
+                       .order_by(func.to_char(func.lower(SportType.name)))
+                       .offset(skip)
+                       .limit(limit)
+                       .all())
+
         count = db.query(SportType).count()
 
         return {"total": count, "objects": sport_types}
+
+    def _add_filter_to_query(self, sport_type_query, filter):
+        key_words = filter.lower().split()
+        sport_types = (
+            sport_type_query
+            .filter(
+                and_(func.concat(func.concat(func.lower(SportType.name), ' '),
+                                 func.concat(func.lower(SportType.nameKZ), ' '))
+                     .contains(name) for name in key_words)
+            )
+        )
+        return sport_types
 
 
 sport_type_service = SportTypeService(SportType)
