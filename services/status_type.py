bfc88e4540e72862dd19f161f3a1eaa37d06453c
krulwaifu <super.raha2002@gmail.com>
2023-11-17 20:19:03 +0600

    feat: status_type filter

--- services/status_type.py
+++ services/status_type.py
@@ -1,3 +1,5 @@
+from sqlalchemy import and_, func
+
 from models import StatusType
 from schemas import StatusTypeCreate, StatusTypeUpdate
 from .base import ServiceBase
@@ -8,17 +10,34 @@ from sqlalchemy.orm import Session
 
 class StatusTypeService(
         ServiceBase[StatusType, StatusTypeCreate, StatusTypeUpdate]):
-    def get_multi(
-        self, db: Session, skip: int = 0, limit: int = 100
-    ) -> List[StatusType]:
-        status_types = (db.query(StatusType)
+    def get_all(
+        self, db: Session, skip: int = 0, limit: int = 100, filter: str = ''
+    ):
+        status_types = (db.query(StatusType))
+
+        if filter != '':
+            status_types = self._add_filter_to_query(status_types, filter)
+
+        status_types = (status_types
                         .order_by(StatusType.name)
                         .offset(skip)
                         .limit(limit)
                         .all())
+
         count = db.query(StatusType).count()
 
         return {"total": count, "objects": status_types}
 
+    def _add_filter_to_query(self, status_type_query, filter):
+        key_words = filter.lower().split()
+        status_types = (
+            status_type_query
+            .filter(
+                and_(func.concat(func.concat(func.lower(StatusType.name), ' '),
+                                 func.concat(func.lower(StatusType.nameKZ), ' '))
+                     .contains(name) for name in key_words)
+            )
+        )
+        return status_types
 
 status_type_service = StatusTypeService(StatusType)
