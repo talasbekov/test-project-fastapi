da27c40fc42780bd992754a02b13182fcea2c616
krulwaifu <super.raha2002@gmail.com>
2024-01-23 16:32:30 +0600

    fix: abroad_travel vehicle_type changed to id

--- schemas/additional/abroad_travel.py
+++ schemas/additional/abroad_travel.py
@@ -4,14 +4,13 @@ from typing import Optional
 
 from pydantic import AnyUrl
 
-from schemas import Model, ReadModel
+from schemas import Model, ReadModel, VehicleTypeRead
 
 from .country import CountryRead
 
 
 class AbroadTravelBase(Model):
-    vehicle_type: str
-    vehicle_typeKZ: str
+    vehicle_type_id: str
     destination_country_id: str
     date_from: datetime
     date_to: Optional[datetime]
@@ -36,5 +35,5 @@ class AbroadTravelUpdate(AbroadTravelBase):
 class AbroadTravelRead(AbroadTravelBase, ReadModel):
     document_link: Optional[str]
     destination_country: Optional[CountryRead]
-    vehicle_typeKZ: Optional[str]
+    vehicle_type: Optional[VehicleTypeRead]
     reasonKZ: Optional[str]
