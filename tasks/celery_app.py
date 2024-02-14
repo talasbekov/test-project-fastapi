9bdb07baa3122f55aa9f847ad4897693df9ee397
xbtio <yernar.qshqr01@gmail.com>
2024-02-14 16:54:20 +0600

    test

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -206,13 +206,13 @@ def task_sign_document_with_certificate(
     # return hr_documents
 
 @app.task(bind=True)
-async def check_expiring_documents(self):
+def check_expiring_documents(self):
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = SessionLocal()
     contracts = history_service.get_expiring_contracts(db)
 
     for contract in contracts:
-        await hr_document_service.send_expiring_notification(db, contract.user_id, contract.id)
+        hr_document_service.send_expiring_notification(db, contract.user_id, contract.id)
     try:
         db.commit()
     except SQLAlchemyError as e:
