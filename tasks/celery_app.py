c767ab57c421f7e2f3db55e97029a894a3634fd1
xbtio <yernar.qshqr01@gmail.com>
2024-02-14 17:59:56 +0600

    test

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -212,8 +212,8 @@ def task_sign_document_with_certificate(
             db.close()
     # return hr_documents
 
-@async_task(app, bind=True)
-async def check_expiring_documents(self):
+@app.task(app, bind=True)
+def check_expiring_documents(self):
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = SessionLocal()
     contracts = history_service.get_expiring_contracts(db)
